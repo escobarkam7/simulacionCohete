6545646ae9451e72f674353a117354be129a126a
Kamila <saikimimundo@outlook.es>
2024-10-14 16:05:50 -0600

    Segundo commit: añadir los datos del cohete como su velocidad inicial y su ángulo de lanzamiento (y convertirlo a radianes)

--- rocketrySimulation.py
+++ rocketrySimulation.py
@@ -5,5 +5,12 @@ import matplotlib.pyplot as plt
 g0 = 9.81 #m/s^2
 R = 6371e3 #Radio de la tierra (m)
 C_d = .75 #Coeficiente de arrastre
-A = 0.01 #Área de la sección transversal (m^2)
+A = 0.01 #Área de la sección transversal del cohete (m^2)
 rho = 1.225 #(kg/m^3)
+
+# Parámetros iniciales
+v0 = 500  #Velocidad inicial (m/s)
+theta = 45  #Ángulo de lanzamiento en grados
+theta_rad = np.radians(theta) #Conversión del ángulo a radianes
+
+

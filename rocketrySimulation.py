78626c688f36d633bee331f28010e1bcfbefd8be
Kamila <saikimimundo@outlook.es>
2024-10-14 15:40:51 -0600

    Primer commit

---
+++ rocketrySimulation.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Declarar constantes
+g0 = 9.81 #m/s^2
+R = 6371e3 #Radio de la tierra (m)
+C_d = .75 #Coeficiente de arrastre
+A = 0.01 #Área de la sección transversal (m^2)
+rho = 1.225 #(kg/m^3)

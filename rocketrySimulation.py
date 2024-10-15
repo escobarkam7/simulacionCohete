616de56f3aff11b47a43d210fec4334cd3ba0f44
Kamila <saikimimundo@outlook.es>
2024-10-14 21:06:40 -0600

    Sexto commit: Obtención de gráfica parabólica que muestra la trayectoria del cohete mostrando como alcanza una altura máxima y regresa al suelo

--- rocketrySimulation.py
+++ rocketrySimulation.py
@@ -7,6 +7,7 @@ R = 6371e3 #Radio de la tierra (m)
 C_d = .75 #Coeficiente de arrastre
 A = 0.01 #Área de la sección transversal del cohete (m^2)
 rho = 1.225 #(kg/m^3)
+m = 1000 #Masa del cohete (kg)
 
 # Parámetros iniciales
 v0 = 500  #Velocidad inicial (m/s)
@@ -30,26 +31,38 @@ v = v0 #v0
 x = 0 #pos horizontal
 y = 0 #pos vertical
 
+#Inicializar componentes de velocidad
+vx = v0 * np.cos(theta_rad) #comp hor
+vy = v0 * np.sin(theta_rad) #comp ver
+
 #Listas para almacenar valores
 tiempos = []
 alturas = []
 posiciones_x = []
 
 #Bluce de simulación
-while h >= 0:
+#Cambiar h a y
+while y >= 0: #La simulación acaba cuando el cohete toca el suelo
     #Guardar los datos en las listas
     tiempos.append(t)
-    alturas.append(h)
+    alturas.append(y)
     posiciones_x.append(x)
 
     #Calcular fuerzas
-    g = gravedad(h) #Gravedad en la altura actual
-    F_res = resistencia(v) #F de resistencia del aire
+    g = gravedad(y) #Gravedad en la altura actual
+    F_res_x = resistencia(vx) #F de resistencia del aire en x y en y
+    F_res_y = resistencia(vy)
 
     #Ecuaciones de movimiento para actualizar las variables
-    v = v - (g + F_res) * dt 
-    h = h + v * np.sin(theta_rad) * dt
-    x = x + v * np.cos(theta_rad) * dt #Pos horizontal 
+
+    #Usando la segunda ley de Newt a = F / m
+    a_x = -F_res_x / m #Aceleración en x
+    a_y = -g - (F_res_y / m) #Aceleración en y
+    #Actualizar componentes de velocidades en función de la aceleración
+    vx += a_x * dt
+    vy += a_y * dt
+    x += vx * dt  #Pos horizontal 
+    y += vy * dt  #Pos vertical 
     t += dt
 
 #Graficar
